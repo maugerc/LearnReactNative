300cf948aec24be5ba729f135d2cebe40f7275cb
Mauger Clément <mauger.clement@gmail.com>
2021-10-25 16:59:43 +0200

    Add button style

--- src/App/components/Button/Button.tsx
+++ src/App/components/Button/Button.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-
-interface Props{
-  text: string;
-}
+import {StyleSheet, Text, View} from 'react-native';
+import {Props} from './Props';
 
 const Button: React.FC<Props> = props => {
   console.log(props);
   return (
-    <View>
-      <Text>{props.text}</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>{props.text}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'dodgerblue',
+    padding: 20,
+    marginTop: 5,
+    marginHorizontal: 20,
+    borderRadius: 10,
+    borderColor: 'mediumblue',
+    borderWidth: 3,
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 18,
+  },
+});
 
 export default Button;
