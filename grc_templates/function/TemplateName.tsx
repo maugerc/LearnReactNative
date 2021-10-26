7fe343ab10c3c25bdc71e670a212c1b46b0da739
Mauger Clément <mauger.clement@gmail.com>
2021-10-26 14:28:12 +0200

    Add template name cli

---
+++ grc_templates/function/TemplateName.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {Props} from './TemplateName.props';
+import style from './TemplateName.style';
+
+const TemplateName: React.FC<Props> = (props: Props) => {
+  return (
+    <View>
+      <Text>templateName</Text>
+    </View>
+  );
+};
+
+export default TemplateName;
